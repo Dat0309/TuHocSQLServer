2fb173ec2b2df6df195089ee07c271c4b9b604f1
Đinh Trọng Đạt <71766267+Dat0309@users.noreply.github.com>
2021-02-07 11:24:25 +0700

    bổ sung buổi 2

---
+++ Truy vấn có điều kiện.sql
@@ -0,0 +1,31 @@
+﻿--Truy vấn kết hợp điều kiện
+--Dùng WHERE giống như IF
+USE HowKteam
+GO
+
+SELECT gv.MAGV, gv.HOTEN,nt.TEN FROM dbo.GIAOVIEN AS gv ,dbo.NGUOITHAN AS nt
+WHERE gv.MAGV =nt.MAGV
+
+--Tìm tên giáo viên quản lý đề tài 
+SELECT gv.MAGV,gv.HOTEN,dt.TENDT,dt.GVCNDT FROM dbo.DETAI AS dt,dbo.GIAOVIEN AS gv
+WHERE dt.GVCNDT=gv.MAGV.
+
+--Tìm tên đề tài tương ứng với tên chủ đề của đề tài đó
+SELECT dt.TENDT,dt.MADT,cd.TENCD FROM dbo.DETAI AS dt,dbo.CHUDE AS cd
+WHERE dt.MACD=cd.MACD
+
+--Lấy ra giáo viên lớn hơn 50 tuổi
+SELECT * FROM dbo.GIAOVIEN
+WHERE YEAR(GETDATE())-YEAR(NGSINH)>50
+
+--Lấy ra giáo viên là trưởng bộ môn
+SELECT gv.* FROM dbo.GIAOVIEN AS gv, dbo.BOMON AS bm
+WHERE gv.MAGV=bm.TRUONGBM
+
+--COUNT(*) đếm số lượng tất cả record
+--COUNT(tên trường cần đếm)
+SELECT COUNT(*) AS N'Số lượng đề tài' FROM dbo.DETAI
+--AVG(Tên column) dùng để tính giá trị trung bình của một cột có giá trị số
+SELECT AVG(LUONG) AS 'TBLUONG' FROM dbo.GIAOVIEN
+--SUM(tên column) là hàm tính tổng giá trị cảu một cột 
+--Lấy tên giáo viên và tên đề tài người đó tham gia khi mà người đó tham gia nhiều hơn 1 lần, dùng truy vấn lồng
---
+++ Tìm kiếm gần đúng, Inner Join và Full Outer Join.sql
@@ -0,0 +1,54 @@
+﻿USE HowKteam
+GO
+
+--Tìm kiếm gần đúng
+--Dùng Like để tìm kiếm gần đúng
+--Đặt dấu % ở trướng hoặc ở sau tùy theo nhu cầu tìm kiếm, vd
+--Tìm tên kết thúc có chữ h
+SELECT * FROM dbo.GIAOVIEN
+WHERE HOTEN LIKE '%h'
+--Tìm người tên Sơn
+SELECT * FROM dbo.GIAOVIEN
+WHERE HOTEN LIKE N'%Sơn'
+
+
+--inner Join kiểu cũ là những tao tác truy vấn (select) đi kèm với điều kiện
+SELECT * FROM dbo.GIAOVIEN
+WHERE HOTEN LIKE '%h'
+
+--inner join kiểu đúng: inner join sẽ tạo ra một vùng chứa các trường có thuộc tính giống nhau (khóa chính, khóa ngoại)
+SELECT *FROM dbo.GIAOVIEN INNER JOIN dbo.BOMON
+ON BOMON.MABM = GIAOVIEN.MABM
+--Join kết hợp điều kiện where
+SELECT GV.MAGV,GV.HOTEN,GV.PHAI,BM.TENBM, K.TENKHOA
+FROM dbo.BOMON AS BM
+     JOIN dbo.GIAOVIEN AS GV ON GV.MABM = BM.MABM
+     JOIN dbo.KHOA AS K ON K.MAKHOA = BM.MAKHOA
+WHERE GV.PHAI ='NAM'
+--ON sẽ lấy nhũng trường có thuộc tính giống nhau có thể tạo ra điều kiện để truy vấn và nhắc lệnh
+ --ON Sẽ nhắc chúng ta nên dùng những điều kiện nào để truy vấn, không giống như where phải tự nghĩ ra điều kiện
+ --Nên sử dụng inner join!
+
+ --full Outer join
+ --là mệnh đề truy vấn với kết quả trả về là tập hợp tất cả dữ liệu chung và riêng giữa thông qua điều kiện kết hợp 2 bảng
+ --các dữ liệu riêng là các dữ liệu không có điểm trùng khớp với điều kiện thì các trường ở bảng sẽ hiển thị là NULL
+ --Cấu trúc
+ --SELECT <Danh sách column>
+--FROM <Table A> FULL OUTER JOIN <Table B>
+--ON <Điều kiện kết hợp AB>
+--Cần cẩn trọng trong việ sử dụng, do kết quả trả về rất lớn
+--vd: những trường hợp sử dụng outer join, 
+
+--Hiển thị thông tin giáo viên đã chủ nhiệm đề tài và những giáo viên chưa chủ nhiệm đề tài để phân nhiệm vụ
+SELECT GV.MAGV, GV.HOTEN, DT.MADT,DT.TENDT
+FROM dbo.GIAOVIEN AS GV
+FULL OUTER JOIN dbo.DETAI AS DT ON DT.GVCNDT = GV.MAGV
+--Xuất thông tin bộ môn đã có giáo viên dạy và chưa có:
+SELECT BM.MABM, BM.TENBM, GV.MAGV,GV.HOTEN
+FROM dbo.BOMON AS BM
+FULL OUTER JOIN dbo.GIAOVIEN AS GV ON GV.MABM = BM.MABM
+--Xuất giáo viên phái nữ chưa có đề tài nào quản lý
+SELECT dt.MADT,dt.TENDT,dt.GVCNDT,gv.MAGV,gv.HOTEN,gv.PHAI
+FROM dbo.DETAI AS dt 
+FULL OUTER JOIN dbo.GIAOVIEN AS gv ON gv.MAGV=dt.GVCNDT
+WHERE gv.PHAI=N'Nữ'
